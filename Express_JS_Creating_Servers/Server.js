593c9e9ac019f5ab42f49fe871b95e91e11b22ee
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2023-08-04 14:51:52 +0530

    Express JS JSON file demo

--- Express_JS_Creating_Servers/Server.js
+++ Express_JS_Creating_Servers/Server.js
@@ -13,6 +13,11 @@ app.get('/home', (request,response) => {
     response.sendFile(path.join(__dirname,'index.html'));
 })
 
+let models = [{brandID:1, id:1, name:'Swift'},{brandID:2, id:2, name:'alto'},{brandID:3, id:3, name:'balero'},{brandID:4, id:4, name:'Celerio'}];
+app.get('/json',(request, response) => {
+    response.json(models);
+});
+
 app.listen(port, hostname, () => {
     console.log(`Express Server is Started at http://${hostname}:${port}`);
 });
