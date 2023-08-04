ebc647c130f7900f8ff4df754b74defe57ecceb9
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2023-08-04 14:47:16 +0530

    Express JS SensFile demo updated

--- Express_JS_Creating_Servers/Server.js
+++ Express_JS_Creating_Servers/Server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 
 const hostname = "127.0.0.1";
 const port =5000;
@@ -7,6 +8,10 @@ const port =5000;
 app.get('/',(request,response) => {
     response.send(`<h2> Welcome to Express JS</h2>`);
 });
+
+app.get('/home', (request,response) => {
+    response.sendFile(path.join(__dirname,'index.html'));
+})
 
 app.listen(port, hostname, () => {
     console.log(`Express Server is Started at http://${hostname}:${port}`);
